1b41ec28ad56036c5801756562a63c6a685dddff
Alex Lin <alexander.lin.kremer@gmail.com>
2013-08-12 10:29:23 -0700

    added messages model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,16 @@
 class MainController < ApplicationController
   def index
+    @message = Message.order("created_at DESC")
   end
 
   def send_message
     @message = params[:message][:text]
+
+    Message.create(content: @message)
+
+    Pusher['chat'].trigger('new_message', {
+      message: @message
+    })
 
     render nothing: true
   end
